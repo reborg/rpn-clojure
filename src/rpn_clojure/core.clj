6e983bc548e2148d117f9026453938404ad0f341
reborg <reborg@reborg.net>
2012-06-01 06:04:04 +0100

    one step back to stub out the push and go one step lower to test the push

--- src/rpn_clojure/core.clj
+++ src/rpn_clojure/core.clj
@@ -1,6 +1,20 @@
 (ns rpn-clojure.core)
 
+;;(defn push [current] [(\1) [\9]])
+;;(defn pop  [stack])
+;;(defn add  [stack])
+;;(defn sub  [stack])
+;;(defn mul  [stack])
+;;(defn div  [stack])
+;;(def operations1 {\0 push \1 pop \2 add \3 sub \4 mul \5 div})
+
+(def operations #{\0 \1 \2 \3 \4 \5})
+
 (defn clifford [program]
-  (if (#{\0 \1 \2 \3 \4 \5} (first program))
-    (if (and (= \0 (first program)) (= 1 (count program))) "missing argument for push")
-    "clifford does not understand operation 6"))
+  (if (operations (first program))
+    (if (and (= \0 (first program)) (= 1 (count program))) 
+      "missing argument for push"
+      (if (= \0 (first program)) 
+        (push [(rest program) []])))
+    (str "clifford does not understand operation " (first program))))
+
--- test/rpn_clojure/test/core.clj
+++ test/rpn_clojure/test/core.clj
@@ -3,7 +3,13 @@
   (:use [midje.sweet]))
 
 (facts "validating operations"
-  (fact "it only executes known operations"
-        (clifford "678") => "clifford does not understand operation 6")
-  (fact "the number to push is required"
-        (clifford "0") => "missing argument for push"))
+       (fact "it only executes known operations"
+             (clifford "678") => "clifford does not understand operation 6"
+             (clifford "Ze8") => "clifford does not understand operation Z")
+       (fact "the number to push is required"
+             (clifford "0") => "missing argument for push"))
+
+(facts "stack operations" 
+       (fact "the first item in is the first item out"
+             (clifford "091") => 9
+             (provided (push ['(\9 \1) []]) => ['(\1) [\9]])))
